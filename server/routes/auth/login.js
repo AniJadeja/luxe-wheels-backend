18c85504ce8e8e07195016d0b9b6c3359035861a
Aniruddhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-01-21 22:19:06 -0500

    Config : configures login data processing

--- server/routes/auth/login.js
+++ server/routes/auth/login.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { verifyAuthCreds } = require('../../middlewares/index.js');
 
 
-const loginRouter = router.post('/' , (req, res) => {
-
-   
+const loginRouter = router.post('/' , verifyAuthCreds.verifyLoginCreds, (req, res) => { 
   res.status(200).json({ message: 'login endpoint reached.' , errors : {}});
  });
 
