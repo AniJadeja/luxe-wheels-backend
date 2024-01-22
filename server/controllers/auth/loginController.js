a97145ab01385c6438511985e38ebe1ae442dcde
Aniruddhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-01-21 23:13:31 -0500

    Refac : Refactors the login endpoint

---
+++ server/controllers/auth/loginController.js
@@ -0,0 +1,16 @@
+const { isUserEmailPresent, signInUser } = require("../../database/database");
+
+const loginUser = (user, res) => {
+  // check if isUserEmailPresent(email)
+  isUserEmailPresent(user.email)
+    ? // if true, return { error: 'Email already exists' }
+      signInUser(user)
+      ? // returns true, return cookie
+        res.status(200).send({ success: "User Logged In" })
+      : res.status(403).send({ error: "Could not be logged in" })
+    : res.status(401).send({ error: "User does not exist" });
+};
+
+module.exports = {
+  loginUser,
+};
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,9 @@
 const { signUpUser } = require('./auth');
-
+const { loginUser } = require('./auth');
 
 
 
 module.exports = {
-  signUpUser
+  signUpUser,
+  loginUser
 }
