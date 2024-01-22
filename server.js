5831d373a9a066cd4b055618799077a7ed4e967a
Aniruddhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-01-21 21:15:22 -0500

    Config : configures the login router

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 // Routers
-const { pingRouter, signupRouter } = require('./server/routes/index.js');
+const { pingRouter, authRouter } = require('./server/routes/index.js');
 const {pingEndPoint, signUpEndPoint, loginEndPoint} = require('./server/config/endpoints.js');
-const loginRouter = require('./server/routes/auth/login.js');
+
 
 const express = require("express");
 const cors = require("cors"); 
@@ -15,8 +15,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use(pingEndPoint, pingRouter);
-app.use(signUpEndPoint, signupRouter);
-app.use(loginEndPoint, loginRouter);
+app.use(signUpEndPoint, authRouter.signupRouter);
+app.use(loginEndPoint, authRouter.loginRouter);
 app.use((req, res, next) => {
   console.log(`Request received for endpoint: ${req.originalUrl}`);
   next();
