b2507890a0f32e8f3c174e385520bbbcc56ab52b
Aniruddhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-01-24 22:31:06 -0500

    Fix : login not responding when user not found

---
+++ server/controllers/auth/loginController.js
@@ -0,0 +1,34 @@
+const { isUserEmailPresent, signInUser } = require("../../database/database");
+
+
+const loginUser = async (user, res) => {
+  // check if isUserEmailPresent(email)
+  if (await isUserEmailPresent(user.email)) {
+    let isUserSignedIn = await signInUser(user);
+    if (isUserSignedIn) {
+      // returns true, return cookie
+      sendCookie(res, isUserSignedIn.cookie);
+    } else {
+      res.status(403).send({ error: "Could not be logged in" });
+    }
+  }
+  else {
+    res.status(403).send({ error: "User not found" });
+  }
+
+};
+
+
+const sendCookie = (res, cookie) => {
+  res.cookie("userId", cookie.uid, {
+            maxAge: 259200000,
+            httpOnly: true,
+            secure: true,
+          })
+  res.status(200).send({ message : "User logged in successfully"})      
+};
+
+
+module.exports = {
+  loginUser,
+};
