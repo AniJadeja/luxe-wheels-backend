482f02820905dd344b2b2d664c7950ebbcb9a8e2
anirudhdhsinhj <anirudhdhsinhj@gmail.com>
2024-02-02 02:26:41 +0000

    confirm

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { retrieveAllSessions } = require("./server/database/methods/session.js");
 // Routers
 const { pingRouter, authRouter } = require('./server/routes/index.js');
 const {pingEndPoint, signUpEndPoint, loginEndPoint} = require('./server/config/endpoints.js');
@@ -26,10 +27,31 @@ app.use((req, res, next) => {
   console.log(`Request received for endpoint: ${req.originalUrl}`);
   next();
 });
+mongoose.connection.once("connected", () => {
 
+  const req = {
+    get: function(headerName) {
+      // return the value of the specified header
+      console.log("headerName :",headerName)
+      return `local${headerName}:${PORT}`;
+    },
+    protocol: "http"
+  };
 
+  app.listen(PORT, () => {
 
-mongoose.connection.once('connected', () => {
-  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+    // print the server url
+    const hostname = req.get("host");
+    const protocol = req.protocol;
+    console.clear();
+    console.log(`Server is running on ${protocol}://${hostname}`);
+
   });
-  
+});
+
+const logSession = async ( sessionToken ) => {
+ const session = await retrieveAllSessions(sessionToken);
+ console.log(" session : ",session)
+};
+
+
