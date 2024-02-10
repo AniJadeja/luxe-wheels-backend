a3d4ee753252dacb37de30bd0fb06276e875379c
anirudhdhsinhj <anirudhdhsinhj@gmail.com>
2024-02-10 04:16:27 +0000

    Cleans up the router code

--- server/routes/auth/signup.js
+++ server/routes/auth/signup.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {signUpUser} = require('../../controllers/index.js');
-const {verifyAuthCreds} = require('../../middlewares/index.js');
+const { signUpUser } = require("../../controllers/index.js");
+const { verifyAuthCreds } = require("../../middlewares/index.js");
 
-const signupRouter = router.post('/',verifyAuthCreds.verifySignUpCreds , (req, res) => {
-
-    signUpUser(req.body, res);
-  //res.status(200).json({ message: 'signup endpoint reached.' , errors : {}});
- });
+const signupRouter = router.post(
+  "/",
+  verifyAuthCreds.verifySignUpCreds,
+  (req, res) => signUpUser(req.body, res)
+);
 
 module.exports = signupRouter;
