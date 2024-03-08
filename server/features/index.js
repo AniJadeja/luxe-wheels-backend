7cc98fccab6979dde47a332012266ae186eac58b
Anirudhdhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-03-07 23:01:31 -0500

    Configures to export the getFeaturedCars

--- server/features/index.js
+++ server/features/index.js
@@ -1,5 +1,7 @@
 const { userRouter:getUserData  } = require("./getUserProfile");
+const { featuredCarsRouter:getFeaturedCarsRouter } = require("./getFeaturedCars");
 
 module.exports = {
   getUserData,
+  getFeaturedCarsRouter
 };
