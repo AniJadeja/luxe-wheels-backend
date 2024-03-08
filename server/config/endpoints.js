7ef80b36e46ab921ca569f45980a3cc01e994ca8
Anirudhdhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-03-07 20:04:21 -0500

    Adds a cars endpoint

--- server/config/endpoints.js
+++ server/config/endpoints.js
@@ -7,6 +7,7 @@ const authEP = process.env.AUTH_END_POINT;
 const signupEP = process.env.SIGNUP_END_POINT;
 const loginEP = process.env.LOGIN_END_POINT;
 const logOutEP = process.env.LOGOUT_END_POINT;
+const carsEP = process.env.CARS_END_POINT;
 
 const signUpEndPoint = versionEP + authEP + signupEP;
 const loginEndPoint = versionEP + authEP + loginEP;
@@ -17,5 +18,6 @@ module.exports = {
     signUpEndPoint,
     loginEndPoint,
     logOutEndPoint,
-    userEndPoint
+    userEndPoint,
+    carsEP,
 };
